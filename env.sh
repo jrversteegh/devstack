4e5b3f494165dc5e58f2085f062cc9ed61cb8d3d
Jaap Versteegh <j.r.versteegh@gmail.com>
2016-06-09 15:51:38 +0200

    - Added python modules

--- env.sh
+++ env.sh
@@ -2,10 +2,9 @@ curdir=`dirname $BASH_SOURCE`
 curdir=`readlink -f $curdir`
 bindir=$curdir/bin
 libdir=$curdir/lib
-lib64dir=$curdir/lib64
 if [ -z $USING_DEVSTACK ]; then
   export USING_DEVSTACK=1
   export PATH=$bindir:$PATH
-  export LD_LIBRARY_PATH=$lib64dir:$libdir:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$libdir:$LD_LIBRARY_PATH
   export PKG_CONFIG_PATH=$libdir/pkgconfig:$PKG_CONFIG_PATH
 fi
---
+++ src/python_modules/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install --upgrade pip && \
+pip install scipy && \
+pip install matplotlib && \
+pip install ipython
