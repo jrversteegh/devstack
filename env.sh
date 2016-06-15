76612c1d7e67d1702fca8046892a59712c0f1c73
Jaap Versteegh <j.r.versteegh@gmail.com>
2016-06-15 20:47:15 +0200

    - Added XDG_DATA_DIRS to environment

--- env.sh
+++ env.sh
@@ -4,9 +4,11 @@ curdir=`dirname $BASH_SOURCE`
 curdir=`readlink -f $curdir`
 bindir=$curdir/root/bin
 libdir=$curdir/root/lib
+sharedir=$curdir/root/share
 if [ -z $USING_DEVSTACK ]; then
   export USING_DEVSTACK=1
   export PATH=$bindir:$PATH
   export LD_LIBRARY_PATH=$libdir:$LD_LIBRARY_PATH
   export PKG_CONFIG_PATH=$libdir/pkgconfig:$PKG_CONFIG_PATH
+  export XDG_DATA_DIRS=$sharedir:$XDG_DATA_DIRS:/usr/local/share/:/usr/share/
 fi
